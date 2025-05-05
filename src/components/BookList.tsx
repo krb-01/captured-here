9b4bfc75d8752022c9d99ff0b5e17d03641db728
dev-team <dev-team@kairiku-books.com>
2025-05-05 13:36:19 +0000

    fix error

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,53 +1,92 @@
-import booksData from '@/lib/books.json';
+import booksData from "@/lib/books.json";
+import { useState, useEffect } from "react";
 
 const BookList = () => {
-  const sortedBooks = [...booksData].sort(
-    (a, b) => new Date(a.published_on).getTime() - new Date(b.published_on).getTime()
+  const [books, setBooks] = useState(
+    [...booksData]
+      .sort(
+        (a, b) =>
+          new Date(a.published_on).getTime() -
+          new Date(b.published_on).getTime()
+      )
+      .map((book) => ({ ...book, hasError: false }))
   );
+
+  useEffect(() => {
+    const checkImages = async () => {
+      const updatedBooks = await Promise.all(
+        books.map(async (book) => {
+          if (book.image_url) {
+            try {
+              const response = await fetch(book.image_url);
+              if (!response.ok) {
+                return { ...book, hasError: true };
+              }
+            } catch (error) {
+              return { ...book, hasError: true };
+            }
+          } else {
+            return { ...book, hasError: true };
+          }
+          return book;
+        })
+      );
+      setBooks(updatedBooks);
+    };
+    checkImages();
+  }, []);
+
   return (
-    <div className="pb-16"><h2 className="text-2xl font-bold text-white pt-8 mb-8">Book List</h2>
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
-      {sortedBooks.map((book) => (
-        <div key={book.isbn} className="relative flex flex-col justify-between rounded-lg bg-white p-4 shadow-sm cursor-pointer">
-        <div className="h-full flex-1 bg-white">
-
-          <div className="mb-4">
-          {book.image_url ? (
-            <img 
-              src={book.image_url}
-              alt={book.title}              
-              className="w-full aspect-square object-contain rounded-md"
-              onError={(e: any) => {
-                e.target.onerror = null;
-                e.target.src = "/file.svg"; 
-              }}            
-            />
-            ) : (
-            <div className="w-full aspect-square bg-gray-200 rounded-md flex justify-center items-center mb-2">
-              <p className="text-gray-500">No image available</p>
+    <div className="pb-16">
+      <h2 className="text-2xl font-bold text-white pt-8 mb-8">Book List</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
+        {books.map((book) => (
+          <div
+            key={book.isbn}
+            className="relative flex flex-col justify-between rounded-lg bg-white p-4 shadow-sm"
+          >
+            <div className="h-full flex-1 bg-white">
+              <div className="mb-4">
+                {book.image_url && !book.hasError ? (
+                  <img
+                    src={book.image_url}
+                    alt={book.title}
+                    className="w-full aspect-square object-contain rounded-md"
+                    onError={(e: any) => {
+                      console.log("Image load error in Booklist");
+                      e.target.onerror = null;
+                      e.target.src = "/file.svg";
+                    }}
+                  />
+                ) : (
+                  <div className="w-full aspect-square object-contain rounded-md">
+                    <img
+                      src="/file.svg"
+                      alt="fallback"
+                      className="w-full h-full object-contain"
+                    />
+                  </div>
+                )}
+              </div>
+              <div className="mb-2">
+                <p className="font-bold text-black">{book.title}</p>
+                <p className="text-sm text-black">by {book.author}</p>
+                <p className="text-xs text-black mt-1">
+                  Published: {new Date(book.published_on).getFullYear()}
+                </p>
+              </div>
             </div>
-          )}
+            <div className="grid grid-cols-2 gap-2 mt-4 ">
+              <button className="bg-black text-white text-xs py-2 px-4 rounded-md hover:scale-105 transition-transform">
+                EXPLORE
+              </button>
+              <button className="bg-amber-500 text-white text-xs py-2 px-4 rounded-md hover:scale-105 transition-transform">
+                AVAILABLE ON AMAZON
+              </button>
+            </div>
           </div>
-          <div className="mb-2">
-            <a href={`/book/${book.isbn}`} className="block">
-              <p className="font-bold text-black">{book.title}</p>
-            </a>
-            <p className="text-sm text-black">by {book.author}</p>
-            <p className="text-xs text-black mt-1">
-              Published: {new Date(book.published_on).getFullYear()}
-            </p>            
-          </div>        
-        </div>  
-          <div className="grid grid-cols-2 gap-2 mt-4 ">
-
-            <button className="text-xs hover:scale-105 transition-all duration-200 bg-[#212121] text-white px-4 py-2 rounded ">EXPLORE</button>
-            <button className="text-xs hover:scale-105 transition-all duration-200 bg-[#FF9900] text-white px-4 py-2 rounded">AVAILABLE ON AMAZON</button>
-          </div>
-
-
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/NewBooks.tsx
+++ src/components/NewBooks.tsx
@@ -1,52 +1,87 @@
-import booksData from "@/lib/books.json";
+import booksData from '@/lib/books.json';
+import { useState, useEffect } from 'react';
 
 const NewBooks = () => {
-  const newBooks = booksData.slice(0, 4);
+  const [newBooks, setNewBooks] = useState(
+    booksData.slice(0, 4).map((book) => ({ ...book, hasError: false }))
+  );
+
+  useEffect(() => {
+    const checkImages = async () => {
+      const updatedBooks = await Promise.all(
+         newBooks.map(async (book) => {
+          if (book.image_url) {
+            try {
+              const response = await fetch(book.image_url);
+              if (!response.ok) {
+                console.error(`Image load failed: ${book.image_url}`);
+                return { ...book, hasError: true };
+              }
+            } catch (error) {
+              console.error(`Image load failed: ${book.image_url}`, error);
+              return { ...book, hasError: true };
+            }
+          }
+          return book;
+        })
+      );
+      setNewBooks(updatedBooks);
+    };
+
+    
+    checkImages();
+  }, [newBooks]);
 
   return (
     <div className="pb-16">
       <h2 className="text-2xl font-bold pt-8 mb-8 text-black">New Arrivals</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
         {newBooks.map((book) => (
-          <div key={book.isbn}>
-            <div className="relative rounded-lg p-4 cursor-pointer border border-gray-300 flex flex-col justify-between h-full bg-white">
-              {book.image_url ? (
-                <div className="mb-4">
+          <div
+            key={book.isbn}
+            className="relative rounded-lg p-4 border border-gray-300 flex flex-col justify-between h-full bg-white"
+          >
+            <div className="mb-4">
+              {book.image_url && !book.hasError ? (
+                <img
+                  src={book.image_url}
+                  alt={book.title}
+                  className="w-full aspect-square object-contain rounded-md"
+                  onError={() => {
+                    setNewBooks((prevBooks) =>
+                      prevBooks.map((b) => (b.isbn === book.isbn ? { ...b, hasError: true } : b))
+                    );
+                  }}
+                />
+                ) : book.image_url ? (
                   <img
-                    src={book.image_url}
-                    alt={book.title}
+                    src="/file.svg"
+                    alt="Fallback"
                     className="w-full aspect-square object-contain rounded-md"
-                    onError={(e: any) => {
-                      e.target.onerror = null; // prevent infinite loop
-                      e.target.src = "/file.svg"; // replace with your default image
-                      e.target.alt = "No image available";
-                      if (e.target.alt === "No image available") {
-                        e.target.className =
-                          "w-full h-48 bg-gray-200 rounded-md mb-2 object-contain flex justify-center items-center";
-                      }
-                    }}
                   />
-                </div>
-              ) : (
-                <div className="w-full h-48 bg-gray-200 rounded-md mb-2 flex justify-center items-center">
-                  <p className="text-gray-500">No image available</p>
-                </div>
+                ) : (
+                  <div className="w-full h-48 bg-gray-200 rounded-md mb-2 flex justify-center items-center"><p className="text-gray-500">No image available</p></div>
+                
               )}
-              <div className="mb-2 flex-1 ">
-                <p className="font-bold text-black">{book.title}</p>
-                <p className="text-sm text-black">by {book.author}</p>
-                <p className="text-xs text-black mt-1">
-                  Published: {new Date(book.published_on).getFullYear()}
-                </p>
-              </div>
-              <div className="grid grid-cols-2 gap-2 mt-4">
-                <button className="bg-[#212121] text-white px-4 py-2 rounded text-xs hover:scale-105 transition-all duration-200">
-                  EXPLORE
-                </button>
-                <button className="bg-[#FF9900] text-white px-4 py-2 rounded text-xs hover:scale-105 transition-all duration-200">
-                  AVAILABLE ON AMAZON
-                </button>
-              </div>
+            </div>
+            <div className="mb-2 flex-1 ">
+              <p className="font-bold text-black">{book.title}</p>
+              <p className="text-sm text-black">by {book.author}</p>
+              <p className="text-xs text-black mt-1">
+                Published: {new Date(book.published_on).getFullYear()}
+              </p>
+            </div>         
+ <div className="grid grid-cols-2 gap-2 mt-4">
+              <button
+                className="bg-black text-white px-4 py-2 rounded text-xs hover:scale-105 transition-all duration-200"
+              >
+                EXPLORE
+              </button>
+              <button
+                className="bg-[#FF9900] text-white px-4 py-2 rounded text-xs hover:scale-105 transition-all duration-200"
+              >
+                AVAILABLE ON AMAZON
+              </button>
             </div>
           </div>
         ))}
