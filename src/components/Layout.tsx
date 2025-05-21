f1437750c62f87fee854969593aa243ad4203f9a
dev-team <dev-team@kairiku-books.com>
2025-05-21 16:56:02 +0000

    fix subtile style

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,10 +10,10 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen bg-white">
       <header className="w-full text-white sticky top-0 z-50 p-4 bg-[#212121]">
-        <Link href="/" className="block sm:inline-block sm:flex-col text-center sm:text-left">
+        <Link href="/" className="block sm:inline-block sm:flex-col text-left">
           {/* Restored to previous Tailwind responsive font size classes */}
           <div className="font-bold text-4xl sm:text-5xl whitespace-nowrap">CAPTURED HERE</div>
-          <div className="text-xs sm:text-sm sm:pl-1 tracking-widest whitespace-nowrap">Explore the World Through Photography Art Books</div>
+          <div className="text-xs tracking-wide pl-0.5 sm:text-sm sm:pl-1 sm:tracking-widest">Explore the World Through Photography Art Books</div>
         </Link>
       </header>
 
