1b300a14df6c583a9c06099cfa8ca6353f4bcb9e
dev-team <dev-team@kairiku-books.com>
2025-06-08 04:57:06 +0000

    fix yml

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   reactStrictMode: true,
-  // images: { // 必要であればコメントを解除
-  //   unoptimized: true,
-  // },
+  images: {
+    unoptimized: true,
+  },
   // 他に必要なNext.jsの設定があればここに追加
 };
 
